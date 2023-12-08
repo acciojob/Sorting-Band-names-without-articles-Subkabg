f23f055951614979acb51b748205dcf0b9fd5edf
Subikabg <92627736+Subkabg@users.noreply.github.com>
2023-12-08 16:06:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //your code here
+document.addEventListener("DOMContentLoaded", function () {
+  // Prompt user for input
+  let bandNames = prompt("Enter band names separated by commas:").split(',');
+
+  // Sort the array without articles
+  bandNames.sort((a, b) => {
+    const removeArticles = (name) => name.replace(/^(a|an|the)\s+/i, '');
+    return removeArticles(a).localeCompare(removeArticles(b));
+  });
+
+  // Create the list inside the ul tag
+  const ul = document.getElementById('bands');
+  bandNames.forEach((band) => {
+    const li = document.createElement('li');
+    li.textContent = band.trim(); // Trim to remove leading/trailing spaces
+    ul.appendChild(li);
+  });
+});
+
 
